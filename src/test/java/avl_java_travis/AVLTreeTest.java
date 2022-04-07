5fad500d170dda776547de0f77729b61f614a6eb
gptshubham595 <gptshubham595@gmail.com>
2022-04-08 03:57:11 +0530

    incr codecov

--- src/test/java/avl_java_travis/AVLTreeTest.java
+++ src/test/java/avl_java_travis/AVLTreeTest.java
@@ -1,12 +1,53 @@
 package test.java.avl_java_travis;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 import main.java.avl_java_travis.AVLTree;
 
 public class AVLTreeTest {
+	@Test
+	public void testFind() {
+		// this array in this order allows to pass in all branches
+		// of the insertion algorithm
+		int[] array = { 10, 20, 30, 40, 50, 25 };
+		AVLTree tree = new AVLTree();
+
+		for (int i = 0; i < array.length; i++) {
+			tree.insert(array[i]);
+		}
+		assertEquals(tree.find(60), null);
+	}
+
+	@Test
+	public void testFind2() {
+		// this array in this order allows to pass in all branches
+		// of the insertion algorithm
+		int[] array = { 10, 20, 30, 40, 50, 25 };
+		AVLTree tree = new AVLTree();
+
+		for (int i = 0; i < array.length; i++) {
+			tree.insert(array[i]);
+		}
+		assertEquals(tree.find(80), null);
+	}
+
+	@Test
+	public void testFind3() {
+		// this array in this order allows to pass in all branches
+		// of the insertion algorithm
+		int[] array = { 10, 20, 30, 40, 50, 25 };
+		AVLTree tree = new AVLTree();
+
+		for (int i = 0; i < array.length; i++) {
+			tree.insert(array[i]);
+		}
+		assertNotNull(tree.find(10));
+
+	}
+
 	@Test
 	public void testRoot() {
 		// this array in this order allows to pass in all branches
@@ -35,6 +76,34 @@ public class AVLTreeTest {
 	}
 
 	@Test
+	public void testTreeHeight2() {
+		// this array in this order allows to pass in all branches
+		// of the insertion algorithm
+		int[] array = { 11, 22, 33, 44, 55, 30 };
+		AVLTree tree = new AVLTree();
+
+		for (int i = 0; i < array.length; i++) {
+			tree.insert(array[i]);
+		}
+
+		assertEquals(tree.height(), 2);
+	}
+
+	@Test
+	public void testTreeHeight3() {
+		// this array in this order allows to pass in all branches
+		// of the insertion algorithm
+		int[] array = { 12, 23, 34, 45, 56, 31 };
+		AVLTree tree = new AVLTree();
+
+		for (int i = 0; i < array.length; i++) {
+			tree.insert(array[i]);
+		}
+
+		assertEquals(tree.height(), 2);
+	}
+
+	@Test
 	public void testTreeDelete() {
 		// this array in this order allows to pass in all branches
 		// of the insertion algorithm
@@ -49,6 +118,35 @@ public class AVLTreeTest {
 	}
 
 	@Test
+	public void testTreeDelete2() {
+		// this array in this order allows to pass in all branches
+		// of the insertion algorithm
+		int[] array = { 10, 20, 30, 40, 50, 25 };
+		AVLTree tree = new AVLTree();
+
+		for (int i = 0; i < array.length; i++) {
+			tree.insert(array[i]);
+		}
+		tree.delete(30);
+		assertNotNull(tree.find(10));
+//		assertEquals(tree.find(10), null);
+	}
+
+	@Test
+	public void testTreeDelete3() {
+		// this array in this order allows to pass in all branches
+		// of the insertion algorithm
+		int[] array = { 10, 20, 30, 40, 50, 25 };
+		AVLTree tree = new AVLTree();
+
+		for (int i = 0; i < array.length; i++) {
+			tree.insert(array[i]);
+		}
+		tree.delete(10);
+		assertNotNull(tree.find(20));
+	}
+
+	@Test
 	public void testTreeBalance() {
 		// this array in this order allows to pass in all branches
 		// of the insertion algorithm
@@ -60,6 +158,20 @@ public class AVLTreeTest {
 		}
 
 		assertEquals(tree.getBalance(tree.find(30)), 0);
+	}
+
+	@Test
+	public void testTreeBalance2() {
+		// this array in this order allows to pass in all branches
+		// of the insertion algorithm
+		int[] array = { 10, 20, 30, 40, 50, 25 };
+		AVLTree tree = new AVLTree();
+
+		for (int i = 0; i < array.length; i++) {
+			tree.insert(array[i]);
+		}
+
+		assertEquals(tree.getBalance(tree.find(60)), 0);
 	}
 
 }
